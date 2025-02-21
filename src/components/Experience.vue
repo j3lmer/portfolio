60d61a80bccc856ab7d20d270c53b96dd2cccb14
j3lmer <jelmer339@gmail.com>
2025-02-21 17:23:06 +0100

    update experiences

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,27 +1,49 @@
 <template>
   <div class="bg-primary min-h-screen flex flex-col justify-center items-center">
     <div class="text-center mb-8 px-5">
-      <h1 class="text-4xl font-bold text-white main-text">Ervaringen</h1>
+      <h1 class="text-4xl font-bold text-white main-text experience-text mb-5">Ervaringen</h1>
     </div>
     <div class="gap-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 px-5 py-5">
-      <div class="card bg-customwhite shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-        <Allprint class="w-full"/>
+      <div
+        class="card bg-allprint shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+
+        <div
+          class="card bg-really_white shadow-xl p-6 mb-2 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300">
+
+          <div class="card-content">
+            <Allprint class="w-full" />
+          </div>
+        </div>
         <div class="card-content">
           <h2 class="mb-2 font-bold text-xl mt-5">Allprint</h2>
-          <span class="text-subtext">Part-time | Heden</span>
+          <span class="text-customwhite">Part-time | Heden</span>
           <p>Specialistische drukkerij.</p>
         </div>
       </div>
-      <div class="card bg-customwhite shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-        <img src="../assets/images/mobilox.png"/>
+      <div
+        class="card bg-mobilox shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        <div
+          class="card bg-really_white shadow-xl p-6 mb-2 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300">
+
+          <div class="card-content">
+            <img src="../assets/images/mobilox.png" />
+          </div>
+        </div>
         <div class="card-content">
           <h2 class="mb-2 font-bold text-xl mt-5">Mobilox</h2>
-          <span class="text-subtext">Part-time | Heden</span>
+          <span class="text-customwhite">Part-time | Heden</span>
           <p>Voertuig voorraadbeheer en advertentieplatform.</p>
         </div>
       </div>
-      <div class="card bg-customwhite shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-        <img src="../assets/images/bouw7.png"/>
+      <div
+        class="card bg-bouw7 shadow-2xl p-6 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        <div
+          class="card bg-really_white shadow-xl p-6 mb-2 rounded-lg w-full max-w-xs mx-auto transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300">
+
+          <div class="card-content">
+            <img src="../assets/images/bouw7.png" />
+          </div>
+        </div>
         <div class="card-content">
           <h2 class="mb-2 font-bold text-xl mt-5">Bouw7</h2>
           <span class="text-subtext">Stage | Verleden</span>
@@ -61,7 +83,8 @@ export default {
 }
 
 .card-content {
-  margin-top: auto; /* Pushes the content to the bottom */
+  margin-top: auto;
+  /* Pushes the content to the bottom */
 }
 
 /* Add any custom styles here */
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       secondary: '#350a16',
       primary: '#8ec3fa',
       customwhite: '#E5E1DA',
-      subtext: '#686D76'
+      subtext: '#686D76',
+      really_white: '#FFFFFF',
+      bouw7: '#F3EB5E',
+      mobilox: '#F5821A',
+      allprint: '#B3B6B7',
     },
     extend: {},
   },
